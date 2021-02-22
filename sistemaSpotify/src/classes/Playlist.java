49f005ae83dcab0ab034fc48a4a7955c9fc5ec8a
Raphael <48337836+PhaellZX@users.noreply.github.com>
2021-02-21 23:21:45 -0300

    Add files via upload

---
+++ sistemaSpotify/src/classes/Playlist.java
@@ -0,0 +1,89 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.time.LocalDate;
+
+public class Playlist {
+	
+	private String titulo;
+	private ArrayList<Musica> musica = new ArrayList<>();
+	private LocalDate dataCriacao;
+	
+	public Playlist(String titulo) {
+		this.setTitulo(titulo);
+		setDataCriacao(LocalDate.now());
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public LocalDate getDataCriacao() {
+		return dataCriacao;
+	}
+	
+	public void setDataCriacao(LocalDate dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	public void addMusica(Musica m) {  // ok
+		
+		this.musica.add(m);
+		System.out.println("Musica adicionada!");
+		
+	}
+	
+	public void removerMusica(Musica m) {  // ok
+		
+		this.musica.remove(m);
+		
+	}
+	
+	public void alterarPosicao(Musica m, int novaPosicao) {  // RESOLVER
+		
+		this.musica.set(novaPosicao, m);
+		
+	}
+	
+	public void getMusica(int pos) {
+		
+		System.out.println(this.musica.get(pos).getTitulo());		
+	}
+	
+	public void getMusicas() {  //ok
+		
+		for(Musica m: musica) {
+			
+			System.out.println(m.getTitulo()); 
+			
+		}
+	}
+	
+	public void getTotalMusicas() { // ok
+		
+		System.out.println("♪ Qtd de músicas: " + this.musica.size());
+		
+	}
+	
+	public void getDuracao(int seg) {  // ok
+		
+		Intervalo i = new Intervalo(seg);
+		
+		i.soma(i);
+	}
+	
+	public void reproduz() {  // ok
+		
+		Random random = new Random();
+		
+		Musica tocar = this.musica.get(random.nextInt(musica.size()));
+		
+		tocar.reproduz();
+		System.out.println("♫ " + tocar.getTitulo() + " ♪");
+	}
+}
